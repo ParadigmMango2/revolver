c75bc20d50b2aa4440c74a485ef41f5bbfa7db51
ParadigmMango <jacobjhartt@gmail.com>
2025-10-18 19:46:34 -0600

    added removal of popular communities tab

--- scripts/remover.js
+++ scripts/remover.js
@@ -8,14 +8,17 @@ function update() {
 	chrome.storage.local.get(res => {
 		const selectors = []
 		if (res.home === undefined || res.home)
+			// When testing homepage selectors, remember to test all three popular feeds:
+			// 1. Normal    2. Dynamic v1    3. Dynamic v2
 			selectors.push(
 				"shreddit-feed[reload-url^=\"/svc/shreddit/feeds/home-feed\"]",
 				"shreddit-feed[reload-url^=\"/svc/shreddit/feeds/popular-feed\"]",
 				"#dynamic-feed-main", // dynamic v1 feed + sort dropdowns
 				".main-container:has(> #main-content > #dynamic-feed-main):not(:has(.right-sidebar))", // dynamic v2 feed + sort dropdowns
 				"div.my-xs.mx-2xs > shreddit-async-loader", // home + popular sort dropdowns
-				"shreddit-gallery-carousel", // popular carousel
 				"div.my-xs.mx-2xs ~ hr", // line between dropdowns and feed
+				"shreddit-gallery-carousel", // popular carousel
+				"aside[aria-label=\"Popular Communities\"]",
 			)
 		if (res.subreddits === undefined || res.subreddits)
 			selectors.push(
